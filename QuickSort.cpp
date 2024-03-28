e998bdb3d664778953d366e0c57e5dcd788bac81
kevinnadr <kevin19305.ib@gmail.com>
2024-03-28 19:26:32 +0700

    masukan input

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -6,3 +6,17 @@ int arr[20];
 int cmp_cout = 0; // number of comparasion
 int mov_cout = 0; // number of data movement
 int n; 
+
+void input () {
+    while (true)
+    {
+        cout << "Masukan panjang element array: ";
+        cin >> n;
+
+        if (n <= 20)
+            break;
+        else
+            cout << "\nMaksimum panjang array adalah 20" << endl;
+
+    }
+}
