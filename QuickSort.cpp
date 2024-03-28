6a94fa011479edceeb8810552dd619f8e079018d
kevinnadr <kevin19305.ib@gmail.com>
2024-03-28 19:55:48 +0700

    step 6

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -52,4 +52,11 @@ void q_short(int low, int high)
     pivot = arr [low]; //step 2
     i = low + 1; //step 3
     j = high;    //step 4
+
+    while (i <= j) //step 10
+    {
+        i++; //step6
+        cmp_count++;
+    }
+
 }
