ea9e5caaded2b643d0858e61364f7e5e14514e8a
kevinnadr <kevin19305.ib@gmail.com>
2024-03-28 20:16:28 +0700

    step 12 dan 13

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -71,11 +71,11 @@ void q_short(int low, int high)
         //swap the element at index i whit the element at index j
         swap(i, j);
     }
+    //recursive call to sort the left sub array
+    q_shorth(low, j - 1); //step 12
+
+    //recursive call to sort the right sub array
+
+    q_short(j + 1, high); //step 13
 }
 
-//recursive call to sort the left sub array
-q_shorth(low, j - 1); //step 12
-
-//recursive call to sort the right sub array
-
-q_short(j + 1, high); //step 13
